10daf4b3af128be3182c797ea40c0c8c1ad8c4cd
Anamaria Lungeanu <anamarialungeanu@gmail.com>
2023-01-03 20:37:05 +0200

    Update streamlit_app.py
    added emojis to each text line
    added one more header

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,9 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('\N{Bowl with Spoon} Breakfast Favorites')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('\N{Bowl with Spoon} Omega 3 & Blueberry Oatmeal')
+streamlit.text('\N{Green Salad} Kale, Spinach & Rocket Smoothie')
+streamlit.text('\N{Chicken} Hard-Boiled Free-Range Egg')
+streamlit.text('\N{Avocado}\N{Bread} Avocado Toast')
+streamlit.header('\N{Banana}\N{Strawberry} Build Your Own Fruit Smoothie \N{Kiwi}\N{Grapes}')
